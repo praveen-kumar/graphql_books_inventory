b1abad1383d1fcc7141a2a01166370d1c8d16c87
praveen <praveech@gmail.com>
2021-02-03 07:03:19 +0000

    setting up graphql

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+
+// creates an express application
+const app = express();
+
+// setup the middleware
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+  })
+);
+
+app.listen(4000, () => {
+  console.log("now listening for requests on port 4000");
+});
