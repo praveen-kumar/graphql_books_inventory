776aae3d8042a1faf509079cd6c66bfea7a1fb4c
praveen <praveech@gmail.com>
2021-02-06 08:04:44 +0000

    Adding a front-end, a react-app

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+class App extends Component{
+  render(){
+    return(
+      <div id="main">
+        <h1>I React</h1>
+      </div>
+    )
+  }
+}
+export default App;
